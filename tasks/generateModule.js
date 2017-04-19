48fb58eb5c174cda5a7a061cc7e12b80df59f5ce
Beni <beni.buess@nzz.ch>
2017-04-19 17:49:21 +0200

    makes symbols available as ES module

---
+++ tasks/generateModule.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const symbolsPath = 'symbols/13x13';
+
+const files = fs.readdirSync(symbolsPath);
+const cantonSymbolFileNames = files.filter(file => {
+  return fs.statSync(path.join(symbolsPath, file)).isFile();
+})
+let cantonSymbols = {};
+for (let symbolFileName of cantonSymbolFileNames) {
+  cantonSymbols[symbolFileName.replace('.svg', '')] = fs.readFileSync(path.join(symbolsPath, symbolFileName), { encoding: 'utf8' });
+}
+
+
+const cityFiles = fs.readdirSync(path.join(symbolsPath, 'cities'));
+const citySymbolFileNames = cityFiles.filter(file => {
+  return fs.statSync(path.join(symbolsPath, 'cities', file)).isFile();
+})
+let citySymbols = {};
+for (let symbolFileName of citySymbolFileNames) {
+  citySymbols[symbolFileName.replace('.svg', '')] = fs.readFileSync(path.join(symbolsPath, 'cities', symbolFileName), { encoding: 'utf8' });
+}
+
+let fileContent = `export const cantons = ${JSON.stringify(cantonSymbols, null, 2)}`;
+fileContent += '\n\n';
+fileContent += `export const cities = ${JSON.stringify(citySymbols, null, 2)}`;
+
+fs.writeFileSync('symbols.js', fileContent);
