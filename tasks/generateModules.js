92409711716b249d8ebcda2a756a5ca6bac3ce29
Beni <beni.buess@nzz.ch>
2017-04-27 15:47:54 +0200

    provides symbols as commonjs module as well

--- tasks/generateModules.js
+++ tasks/generateModules.js
@@ -22,8 +22,15 @@ for (let symbolFileName of citySymbolFileNames) {
   citySymbols[symbolFileName.replace('.svg', '')] = fs.readFileSync(path.join(symbolsPath, 'cities', symbolFileName), { encoding: 'utf8' });
 }
 
+
 let fileContent = `export const cantons = ${JSON.stringify(cantonSymbols, null, 2)}`;
 fileContent += '\n\n';
 fileContent += `export const cities = ${JSON.stringify(citySymbols, null, 2)}`;
-
 fs.writeFileSync('symbols.js', fileContent);
+
+let cjsFileContent = `module.exports = {
+  cantons: ${JSON.stringify(cantonSymbols, null, 4)},
+  cities: ${JSON.stringify(citySymbols, null, 4)}
+};
+`;
+fs.writeFileSync('symbols.cjs.js', cjsFileContent);
